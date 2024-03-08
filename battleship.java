8fd95a3c1ee45cd50f5f18124d4f2044475e2e37
alelomelir <alejandralomeli2712@gmail.com>
2024-03-08 14:43:26 -0600

    Add a simple battleship game

--- battleship.java
+++ battleship.java
@@ -1,41 +1,75 @@
 import java.util.Random;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class battleship {
 
     public static void main(String[] args) {
 
-        // int coor1;
-        // int coor2;
-
         Random rand = new Random();
-        int random = rand.nextInt(9);
+        int c1 = rand.nextInt(10);
+        int c2 = rand.nextInt(10);
 
-        int[][] arregloBi = new int[2][2];
-        {
-            int i = 0;
-            int j = 0;
-            for (i = 0; i < 2; i++) {
-                for (j = 0; j < 2; j++) {
-                    arregloBi[i][j] = rand.nextInt(10);
-                }
+        char[][] arregloBi = new char[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                arregloBi[i][j] = '*';
             }
-            int[][] arreglo3 = new int[2][2];
-            for (i = 0; i <= 2; i++) {
-                for (j = 0; j <= 2; j++) {
-                    System.out.println(arregloBi[i][j]); // i y j para que sea dinamico
-                    arreglo3[i][j] = arregloBi[i][j];
-                }
-            }
-            for (i = 0; i <= 2; i++) {
-                for (j = 0; j <= 2; j++) {
-
-                }
-            }
-
-            // Scanner coordenada = new Scanner(System.in);
         }
 
+        arregloBi[c1][c2] = '<'; // guardando el barco en el arreglo bidimensional en las coordenadas aleatorias
+
+        int coor1 = 0;
+        int coor2 = 0;
+
+        Scanner scnr = new Scanner(System.in);
+
+        int vidas = 5;
+        while (vidas > 0) {
+
+            System.out.println("Introduce la coordenada x (letra): ");
+            coor1 = ParseInt(scnr.next().charAt(0));
+
+            // char coor1Chart = ' ';
+            // coor1Chart = scnr.next().charAt(0);
+            // int coor1Num = ParseInt(coor1Chart);
+
+            System.out.println("Introduce la coordenada y (numero): ");
+            coor2 = scnr.nextInt();
+
+            if (arregloBi[coor1][coor2] == '<') {
+                System.out.println("Le atinaste!!!!");
+                vidas = 0;
+            } else {
+                System.out.println("No le atinaste :(");
+                vidas--;
+                System.out.println("Te restan " + vidas + " vidas");
+
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(' ');
+                System.out.print(' ');
+                System.out.print(arregloBi[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int ParseInt(char coordenada) { // se crea una funcion que recibe la coordenada (char) y regresa un
+                                                  // numero
+        char[] aux = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j' }; // arreglo de caracteres, el aux contiene el
+                                                                           // arreglo
+        int temp = 0; // variable para guardar la posicion en la que esta el caracter
+
+        for (int i = 0; i < aux.length; i++) {
+            if (coordenada == aux[i]) {// compara coordenada con aux en la posicion i
+                temp = i + 1; // aumenta uno
+                break;
+            }
+        }
+        return temp; // regresa la posicion
     }
 
 }
